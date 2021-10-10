ddd100522daecfd7bbb42c300288bd443cce2d64
Student User <student@workstation.lab.example.com>
2021-10-10 15:55:04 -0400

    mmw

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,12 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "Another change is coming!\n";
+print "cat /usr/local/etc/ocp4.config\n";
+print "RHT_OCP4_MASTER_API=https://api.na46.prod.nextcle.com:6443\n";
+print "RHT_OCP4_WILDCARD_DOMAIN=apps.na46.prod.nextcle.com\n";
+print "RHT_OCP4_NEXUS_SERVER=nexus-common.apps.na46.prod.nextcle.com\n";
+print "RHT_OCP4_DEV_USER=awupve\n";
+print "RHT_OCP4_DEV_PASSWORD=6a37475208b44006b734\n";
+print "RHT_OCP4_GITHUB_USER=mmwillingham\n";
+print "RHT_OCP4_QUAY_USER=bolauder88\n";
 ?>
